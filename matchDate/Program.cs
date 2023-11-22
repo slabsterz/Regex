555ce2d3c06fcb1fa0e36d4645167455239535da
slabsterz <137103435+slabsterz@users.noreply.github.com>
2023-11-22 16:51:51 +0200

    Add project files.

---
+++ matchDate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+
+string pattern = @"(?<day>\d{2})(?<sep>[\.\-\/])(?<month>[A-Z][a-z]+)(\k<sep>)(?<year>\d{4})\b";
+
+Regex regex = new Regex(pattern);
+
+MatchCollection matches = regex.Matches(text);
+
+foreach (Match match in matches)
+{
+    Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+}
+
+int[] arr = new int[2];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = int.Parse(Console.ReadLine());
+}
---
+++ matchPhone/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+
+string expression = @"\+359(?<sep>[\ \-])2(\k<sep>)\d{3}(\k<sep>)\d{4}\b";
+
+Regex regex = new Regex(expression);
+
+MatchCollection matches = regex.Matches(text);
+
+Console.WriteLine(string.Join(", ", matches));
